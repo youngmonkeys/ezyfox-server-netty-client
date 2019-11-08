b96776959e171e9a64e6aeea2c13ee7d5fcca9ce
Dung Ta Van <itprono3@gmail.com>
2019-11-08 23:08:04 +0700

    update app requests

--- src/test/java/com/tvd12/ezyfoxserver/client/testing/LoginSuccessHandler.java
+++ src/test/java/com/tvd12/ezyfoxserver/client/testing/LoginSuccessHandler.java
@@ -2,13 +2,13 @@ package com.tvd12.ezyfoxserver.client.testing;
 
 import com.tvd12.ezyfox.entity.EzyData;
 import com.tvd12.ezyfoxserver.client.handler.EzyLoginSuccessHandler;
-import com.tvd12.ezyfoxserver.client.request.EzyAccessAppRequest;
+import com.tvd12.ezyfoxserver.client.request.EzyAppAccessRequest;
 
 public class LoginSuccessHandler extends EzyLoginSuccessHandler {
 
 	@Override
 	protected void handleLoginSuccess(EzyData responseData) {
-		client.send(new EzyAccessAppRequest("hello-world"));
+		client.send(new EzyAppAccessRequest("hello-world"));
 	}
 	
 }
